65b6d42603b1a4d6b530c31d9874f8960561b102
Mehrshad Hamavandy <73274226+mehrshud@users.noreply.github.com>
2026-02-26 16:49:36 +0330

    feat: implement core modules and models

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
+import { defineConfig } from 'vite';
+import tailwind from 'vite-plugin-tailwind';
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwind()],
+  build: {
+    outDir: 'build'
+  }
 });
